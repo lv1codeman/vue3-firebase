af9ed7ceab0ac8260e09ee7855375fee2c70a7ff
lv1codeman <lv1codeman@gmail.com>
2024-04-19 15:31:12 +0800

    delete功能

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@ import axios from "axios";
 import { onMounted } from "vue";
 
 // TODO: 列表渲染
+// 聲明響應式list > 調用接口獲取數據 > 後端數據賦值給list > 綁訂到table組件
 const list = ref([]);
 const getList = async () => {
   const res = await axios.get("/list");
@@ -15,6 +16,17 @@ onMounted(() => {
   getList();
 });
 // TODO: 删除功能
+// 獲取當前行的id > 通過id調用刪除接口 > 更新最新的列表
+
+const onDelete = async (row, column, index) => {
+  console.log("sss");
+  console.log(row);
+  console.log(column);
+  console.log(index);
+
+  await axios.delete(`del/${row.id}`);
+  getList();
+};
 
 // TODO: 编辑功能
 </script>
@@ -26,9 +38,11 @@ onMounted(() => {
       <el-table-column label="姓名" prop="name" width="150"></el-table-column>
       <el-table-column label="籍贯" prop="place"></el-table-column>
       <el-table-column label="操作" width="150">
-        <template #default>
+        <template #default="{ row, column, $index }">
           <el-button type="primary" link>编辑</el-button>
-          <el-button type="danger" link>删除</el-button>
+          <el-button type="danger" link @click="onDelete(row, column, $index)"
+            >删除</el-button
+          >
         </template>
       </el-table-column>
     </el-table>
